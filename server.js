3248b038db09ea6cf053952eb23933e7846dc107
Makvana-karan <karanmakwana402@gmail.com>
2024-06-20 13:25:53 +0530

    save all

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import colors from "colors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
@@ -7,32 +6,24 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 
-//configure env
 dotenv.config();
 
-//databse config
 connectDB();
 
-//rest object
 const app = express();
 
-//middelwares
 app.use(cors());
 
-//routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-//rest api
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to ecommerce app</h1>");
 });
 
-//PORT
 const PORT = process.env.PORT || 8080;
 
-//run listen
 app.listen(PORT, () => {
-  console.log(`Server Running on  mode on port ${PORT}`.bgCyan.white);
+  console.log(`Server Running on  mode on port ${PORT}`);
 });
