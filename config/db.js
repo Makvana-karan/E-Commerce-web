1915a21e05b44b447b2486cc162f0b8bc31e9413
Makvana-karan <karanmakwana402@gmail.com>
2024-06-20 13:47:01 +0530

    js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
-import colors from "colors";
+
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(
-      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
-    );
+    console.log("connected To Mongodb Database");
   } catch (error) {
-    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+    console.log(`Error in Mongodb ${error}`);
   }
 };
 
